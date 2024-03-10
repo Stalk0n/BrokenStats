ca053d19184ca448884fcaf333054d54ca8440c0
DScounterGO <aleksander.cyniak1@gmail.com>
2024-03-10 03:30:12 +0100

    Dodaj pliki projektów.

---
+++ BroknStats/Form1Addon1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BroknStats
+{
+
+    public partial class Form1 : Form
+    {
+        public void laugh()
+        {
+            MessageBox.Show("Haha");
+        }
+    }
+
+
+}
+
